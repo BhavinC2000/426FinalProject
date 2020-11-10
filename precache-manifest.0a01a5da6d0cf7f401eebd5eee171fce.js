8f5187f628900d9ad38cdd25e4ac607de1d7570c
Bhavin Chaudhari <bcbchaudhari@gmail.com>
2020-11-10 10:01:59 -0500

    Updates

---
+++ precache-manifest.0a01a5da6d0cf7f401eebd5eee171fce.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc5e71b7aeda3c6ce312a8e4688638a0",
+    "url": "/426FinalProject/index.html"
+  },
+  {
+    "revision": "dbe6bc382a4c753da5b3",
+    "url": "/426FinalProject/static/css/main.7b286c37.chunk.css"
+  },
+  {
+    "revision": "e5a031015f3ccddb88f1",
+    "url": "/426FinalProject/static/js/2.2e1200bb.chunk.js"
+  },
+  {
+    "revision": "70d88436874ad6f0b4ff8dc0e5c19c4e",
+    "url": "/426FinalProject/static/js/2.2e1200bb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dbe6bc382a4c753da5b3",
+    "url": "/426FinalProject/static/js/main.9013fc2c.chunk.js"
+  },
+  {
+    "revision": "e89b9db02dfcec4915ed",
+    "url": "/426FinalProject/static/js/runtime-main.d0e901e9.js"
+  }
+]);
